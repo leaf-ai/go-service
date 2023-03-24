ded6a9713b1b70b140c6360b96f3f0cafb37d584
Karl Mutch <karlmutch@users.noreply.github.com>
2023-03-24 13:34:18 -0700

    Fix runaway labels issue when updating values

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,4 +1,4 @@
-// Copyright 2018-2022 (c) The Go Service Components authors. All rights reserved. Issued under the Apache 2.0 License.
+// Copyright 2018-2023 (c) The Go Service Components authors. All rights reserved. Issued under the Apache 2.0 License.
 
 package log // import "github.com/karlmutch/go-service/pkg/log"
 
@@ -23,7 +23,6 @@ func init() {
 
 // Logger encapsulates the logging device that is used to emit logs and
 // as a receiver that has the logging methods
-//
 type Logger struct {
 	log        logxi.Logger      // The base implementation that is being encapsulated
 	debugStack bool              // Should a debug stack be produced with the message
@@ -34,7 +33,6 @@ type Logger struct {
 
 // NewLogger can be used to instantiate a wrapper logger with a module label with
 // output going stdout
-//
 func NewLogger(component string) (log *Logger) {
 	logxi.DisableCallstack()
 
@@ -54,7 +52,6 @@ func NewLogger(component string) (log *Logger) {
 
 // NewErrLogger can be used to instantiate a wrapper logger with a module label with
 // output going stderr
-//
 func NewErrLogger(component string) (log *Logger) {
 	logxi.DisableCallstack()
 
@@ -73,6 +70,12 @@ func NewErrLogger(component string) (log *Logger) {
 }
 
 func (l *Logger) Label(key string, value string) {
+
+	// Dont allow zero length keys into the set of labels
+	if len(key) == 0 {
+		return
+	}
+
 	l.Lock()
 	defer l.Unlock()
 
@@ -90,7 +93,7 @@ func (l *Logger) Label(key string, value string) {
 		} else {
 			l.included[key] = value
 		}
-		l.labels = make([]string, len(l.labels)*2)
+		l.labels = make([]string, 0, len(l.included)*2)
 		for k, v := range l.included {
 			l.labels = append(l.labels, k)
 			l.labels = append(l.labels, v)
@@ -107,7 +110,6 @@ func (l *Logger) Label(key string, value string) {
 }
 
 // IncludeStack is used to enable a small function call stack to be included with messages
-//
 func (l *Logger) IncludeStack(included bool) (log *Logger) {
 	l.Lock()
 	defer l.Unlock()
@@ -117,7 +119,6 @@ func (l *Logger) IncludeStack(included bool) (log *Logger) {
 }
 
 // HostName is used to add an optional host name to messages, if empty then the host name will not be output
-//
 func (l *Logger) HostName(hostName string) (log *Logger) {
 	l.Label("hostName", hostName)
 	return l
@@ -126,7 +127,6 @@ func (l *Logger) HostName(hostName string) (log *Logger) {
 // Trace is a method for output of trace level messages
 // with a varargs style list of parameters that is formatted
 // as label and then the value in a single list
-//
 func (l *Logger) Trace(msg string, args ...interface{}) {
 	if !l.IsTrace() {
 		return
@@ -152,7 +152,6 @@ func (l *Logger) Trace(msg string, args ...interface{}) {
 // Debug is a method for output of debugging level messages
 // with a varargs style list of parameters that is formatted
 // as label and then the value in a single list
-//
 func (l *Logger) Debug(msg string, args ...interface{}) {
 	if !l.IsDebug() {
 		return
@@ -178,7 +177,6 @@ func (l *Logger) Debug(msg string, args ...interface{}) {
 // Info is a method for output of informational level messages
 // with a varargs style list of parameters that is formatted
 // as label and then the value in a single list
-//
 func (l *Logger) Info(msg string, args ...interface{}) {
 	if !l.IsInfo() {
 		return
@@ -204,7 +202,6 @@ func (l *Logger) Info(msg string, args ...interface{}) {
 // Warn is a method for output of warning level messages
 // with a varargs style list of parameters that is formatted
 // as label and then the value in a single list
-//
 func (l *Logger) Warn(msg string, args ...interface{}) error {
 	if !l.IsWarn() {
 		return nil
@@ -230,7 +227,6 @@ func (l *Logger) Warn(msg string, args ...interface{}) error {
 // Error is a method for output of error level messages
 // with a varargs style list of parameters that is formatted
 // as label and then the value in a single list
-//
 func (l *Logger) Error(msg string, args ...interface{}) error {
 
 	allArgs := append([]interface{}{}, args...)
@@ -251,7 +247,6 @@ func (l *Logger) Error(msg string, args ...interface{}) error {
 // Fatal is a method for output of fatal level messages
 // with a varargs style list of parameters that is formatted
 // as label and then the value in a single list
-//
 func (l *Logger) Fatal(msg string, args ...interface{}) {
 	allArgs := append([]interface{}{}, args...)
 
@@ -271,7 +266,6 @@ func (l *Logger) Fatal(msg string, args ...interface{}) {
 // Log is a method for output of parameterized level messages
 // with a varargs style list of parameters that is formatted
 // as label and then the value in a single list
-//
 func (l *Logger) Log(level int, msg string, args []interface{}) {
 	allArgs := append([]interface{}{}, args...)
 
@@ -292,7 +286,6 @@ func (l *Logger) Log(level int, msg string, args []interface{}) {
 
 // SetLevel can be used to set the threshold for the level of messages
 // that will be output by the logger
-//
 func (l *Logger) SetLevel(lvl int) {
 	l.Lock()
 	defer l.Unlock()
@@ -301,7 +294,6 @@ func (l *Logger) SetLevel(lvl int) {
 
 // IsTrace returns true in the event that the theshold logging level
 // allows for trace messages to appear in the output
-//
 func (l *Logger) IsTrace() bool {
 	l.Lock()
 	defer l.Unlock()
@@ -310,7 +302,6 @@ func (l *Logger) IsTrace() bool {
 
 // IsDebug returns true in the event that the theshold logging level
 // allows for debugging messages to appear in the output
-//
 func (l *Logger) IsDebug() bool {
 	l.Lock()
 	defer l.Unlock()
@@ -319,7 +310,6 @@ func (l *Logger) IsDebug() bool {
 
 // IsInfo returns true in the event that the theshold logging level
 // allows for informational messages to appear in the output
-//
 func (l *Logger) IsInfo() bool {
 	l.Lock()
 	defer l.Unlock()
@@ -328,7 +318,6 @@ func (l *Logger) IsInfo() bool {
 
 // IsWarn returns true in the event that the theshold logging level
 // allows for warning messages to appear in the output
-//
 func (l *Logger) IsWarn() bool {
 	l.Lock()
 	defer l.Unlock()
